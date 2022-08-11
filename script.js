611bddfb4729633d24821a149653fd24c48e506a
Tobias Aagaard <Tobias909@live.dk>
2022-08-11 13:55:50 +0200

    Ændre int til float
    float

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ function calc(form) {
     let valTwo = document.getElementById("value2").value;
 
     if (op === "+") {
-        def = parseInt(valOne) + parseInt(valTwo);
+        def = parseFloat(valOne) + parseFloat(valTwo);
     }
 
     else if (op === "-") {
-        def = parseInt(valOne) - parseInt(valTwo);
+        def = parseFloat(valOne) - parseFloat(valTwo);
     }
 
     else if (op === "*") {
-        def = parseInt(valOne) * parseInt(valTwo);
+        def = parseFloat(valOne) * parseFloat(valTwo);
     }
 
     else if (op === "/") {
-        def = parseInt(valOne) / parseInt(valTwo);
+        def = parseFloat(valOne) / parseFloat(valTwo);
     }
 
     console.log(def)
